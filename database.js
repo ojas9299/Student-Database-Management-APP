0de2411b47cef17ca5e8ec24bc51561240b1fc99
Ojas Bhalerao <meerasbhalerao@gmail.com>
2024-04-01 16:09:50 +0530

    vercel support added

---
+++ database.js
@@ -0,0 +1,53 @@
+let posts = [
+    {   id : "1",
+        name : "Piyush Ghorpade  ",    
+        rollno : "c23328",
+        place  : "Kolhapur",
+    },
+
+    {    id : "2",
+        name : "Ashish Khedkar  ",    
+        rollno : "c23340",
+        place  : "Shirur",
+    },
+
+    {    id : "3",
+        name : "Vaishnav Sagonkar  ",    
+        rollno : "c23339",
+        place  : "Kolhapur",
+    },
+
+    {    id : "4",
+        name : "Ojas Bhalerao  ",    
+        rollno : "c23362",
+        place  : "Pune",
+    },
+
+    {    id : "5",
+        name : "Jagruti Nandre ",    
+        rollno : "c23334",
+        place  : "Shivne",
+    },
+    
+    {    id : "6",
+        name : "Tanishka Deshpande  ",    
+        rollno : "c23357",
+        place  : "Pune",
+    },
+
+    {    id : "7",
+        name : "Srushti Nalawade",    
+        rollno : "c23367",
+        place  : "Pune",
+    },
+    
+    {    id : "8",
+        name : "Sakshi Khomane  ",    
+        rollno : "c23309",
+        place  : "Pune",
+    },
+
+]
+
+
+module.exports = {posts};
